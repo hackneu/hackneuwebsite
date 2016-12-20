1b99ed00c03e948640d267fdc9825964971e216e
Avanti Patil <patilav@ccs.neu.edu>
2016-12-19 22:32:26 -0500

    First version of website

---
+++ js/backgroud.js
@@ -0,0 +1,29 @@
+(function($) {
+    "use strict";
+    // Scroll page
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight top nav bar when user starts scrolling
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function(){ 
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+})(jQuery);
